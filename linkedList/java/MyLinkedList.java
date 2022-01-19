9a32804e19ae70d8676d675178e649c01b168174
ElwoodReadz <panama-2009@live.com>
2022-01-18 20:30:00 -0800

    added push, set, remove and pop

--- linkedList/java/MyLinkedList.java
+++ linkedList/java/MyLinkedList.java
@@ -24,45 +24,44 @@ public class MyLinkedList
     // Create a linked list that contains the data in the provided array
     public MyLinkedList(int[] initialData)
     {
-
+        
     }
     
     // Add a node to the start of the list
     public void push(int newValue)
     {
+        Node new_node = new Node(newValue);
+        //newValue I think idk
+        new_node.next = Head;
+        Head = new_node;
     }
     
     // Add a node at the provided index (such that LinkedList.get(index) == newValue after this call completes)
     public void insert(int data, int index)
     {
         Node new_node = new Node(data);
-        if (index == 0)
-        {
-            new_node.next = head;
-            head = new_node;
-        }
-        else
-        {
-            new_node.next = getNode(index-1).next;
-            getNode(index-1).next = new_node;
-        }
+        new_node.next = getNode(index-1).next;
+        getNode(index-1).next = new_node;
     }
     
     // Remove a node from the front of the list
     public int pop()
     {
-        return 0;
+        Head = Head.next;
+        return Head.Data;
     }
     
     // Remove the node at the specified index
     public int remove(int index)
     {
-        return 0;
+        getNode(index - 1).next = getNode(index).next;
+        return getNode(index).Data;
     }
     
     // Set the value of the node at the specified index
     public void set(int newValue, int index)
     {
+        getNode(index).Data = newValue;
     }
 
     public int get(int index)
